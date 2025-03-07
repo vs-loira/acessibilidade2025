17e0e7335320ca65c1cb2c0a006931b4d865431a
vs-loira <144239563+vs-loira@users.noreply.github.com>
2025-03-07 09:06:32 -0300

    yu

--- script.js
+++ script.js
@@ -19,4 +19,10 @@ document.addEventListener('DOMContentLoaded', function(){
 const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')botaoDeAcessibilidade.addEventListener('click', function (){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
-   })
+   })alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado)ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
